f6a87309e6fb50b97e247cfb724a9c2b428fe546
Daizke Suarez <daizke9911@gmail.com>
2025-11-11 18:55:17 -0400

    SOLGES V1.0

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     
+    server: {
+        host: '192.168.2.3', 
+
+        cors: true, 
+
+        hmr: {
+        }
+    },
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
